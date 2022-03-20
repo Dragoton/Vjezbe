4f60207a808f690e8a378ebe6b66ed9c32c4924f
Drago Tomasic <drag0540@stud.kea.dk>
2022-03-20 21:47:30 +0100

    Close

---
+++ animated_txt_starter_files/script.js
@@ -0,0 +1,10 @@
+"use strict";
+let h3 = document.querySelector("h3");
+
+window.addEventListener("DOMContentLoaded", loaded);
+
+function loaded(wrap) {
+  let cutter = h3.shift();
+}
+
+function spanWrapper() {}
